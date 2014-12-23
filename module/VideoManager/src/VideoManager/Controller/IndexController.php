b5a91fadd6cf005d1d82a0a880f5607d32f260c3
Angela Molina <angela.molina@learnable.com>
2014-12-23 12:20:01 +1100

    Lesson 3.5 - final'

---
+++ module/VideoManager/src/VideoManager/Controller/IndexController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace VideoManager\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        $view = new ViewModel(array(
+            'music' => 'rock',
+            'artist' => 'pearl jam'
+        ));
+
+        $view->setVariable('socialMedia', 'Google+');
+
+        $view->setVariables(array(
+            'networks' => array(
+                'Twitter', 'Google+', 'LinkedIn', 'Facebook'
+            ),
+            'car' => 'Porsche 911'
+        ));
+
+        return $view;
+    }
+
+    public function deleteAction()
+    {
+        return new ViewModel();
+    }
+
+    public function viewAction()
+    {
+        return new ViewModel();
+    }
+
+    public function searchAction()
+    {
+        return new ViewModel();
+    }
+
+    public function manageAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
