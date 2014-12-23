c33db2a4fc2b55ad5615eb370f43ab2e985459f4
Angela Molina <angela.molina@learnable.com>
2014-12-23 12:26:03 +1100

    Lesson 5.3 - final

---
+++ module/VideoManager/src/VideoManager/Controller/IndexController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace VideoManager\Controller;
+
+use VideoManager\Tables\VideoTable;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    protected $videoTable;
+
+    public function __construct(VideoTable $videoTable)
+    {
+        $this->videoTable = $videoTable;
+    }
+
+    public function indexAction()
+    {
+        $view = new ViewModel(array(
+            'music' => 'rock',
+            'artist' => 'pearl jam'
+        ));
+
+        $view->setVariable('socialMedia', 'Google+');
+
+        $view->setVariables(array(
+            'networks' => array(
+                'Twitter', 'Google+', 'LinkedIn', 'Facebook'
+            ),
+            'car' => 'Porsche 911'
+        ));
+        
+        $results = $this->videoTable->fetchMostRecent();
+        print count($results); // print $results->count();
+
+        return $view;
+    }
+
+    public function deleteAction()
+    {
+        $this->layout('video-layout');
+
+        return new ViewModel();
+    }
+
+    public function viewAction()
+    {
+        return new ViewModel();
+    }
+
+    public function searchAction()
+    {
+        return new ViewModel();
+    }
+
+    public function manageAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
